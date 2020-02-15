86df956a31a1fed902d4673125d9c74166db607f
Mohab Ahmed <mohab.ahmed.2032@gmail.com>
2020-02-15 04:21:01 +0200

    adding makefile to simplify things

---
+++ c1/sources.mk
@@ -0,0 +1,13 @@
+#**********************************************************
+#
+# source file for first coursera assignment
+#
+# it contains :
+# 	stats.c
+# 	stats.h
+#
+#**********************************************************
+
+SRC = stats.c
+
+HEADER = stats.h
