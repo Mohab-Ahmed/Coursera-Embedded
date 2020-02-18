e5f9152c03ed831d7d99dfc7a3b6c790b96f0c6e
Mohab Ahmed <mohab.ahmed.2032@gmail.com>
2020-02-18 03:07:40 +0200

    2nd assignment, P.S. only MSP432 work for now

---
+++ c2/src/sources.mk
@@ -0,0 +1,25 @@
+#************************************************
+#
+# sources to our make file which are :
+#	main.c 
+#	memory.c
+#	interrupts_msp432p401r_gcc.c
+#	startup_msp432p401r_gcc.c
+#	system_msp432p401r.c
+# 
+# Include path to header files :
+#	common
+#	CMSIS
+#	msp432
+#
+#************************************************
+	 
+SOURCES  = main.c \
+           memory.c \
+	   interrupts_msp432p401r_gcc.c \
+	   startup_msp432p401r_gcc.c \
+	   system_msp432p401r.c 
+
+INCLUDE1 = ../include/common
+INCLUDE2 = ../include/CMSIS 
+INCLUDE3 = ../include/msp432
