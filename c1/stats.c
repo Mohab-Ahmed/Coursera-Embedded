d74cae9d7dbb118c572ded0c3247faa953fddf93
Mohab Ahmed <mohab.ahmed.2032@gmail.com>
2020-04-30 01:26:14 +0200

    Finalizing c1 content

--- c1/stats.c
+++ c1/stats.c
@@ -15,135 +15,126 @@
 *
 ********************************************************************/
 #include <stdio.h>
-#include<stdlib.h>
 #include"stats.h"
 
 int main()
 {
-    unsigned int data[SIZE] = {34, 201, 190, 154,   8, 194,   2,   6 ,
-                                114, 88,   45,  76, 123,  87,  25,  23,
-                                200, 122, 150, 90,   92,  87, 177, 244,
-                                201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-    print_array(&data[0]);
-    print_statistics(&data[0]) ;
+    uchar_t data[SIZE] ={   34, 201, 190, 154,   8, 194,   2,   6 ,
+                            114, 88,   45,  76, 123,  87,  25,  23,
+                            200, 122, 150, 90,   92,  87, 177, 244,
+                            201,   6,  12,  60,   8,   2,   5,  67,
+                            7,  87, 250, 230,  99,   3, 100,  90};
+    
+    print_array(data);
+    print_statistics(data) ;
+    sort_array(data);
     printf("\nThe new sorted array from large to small will be :\n");
-    sort_array(&data[0]) ;
+    print_array(data);
 
-    return 0 ;
+    return 0;
 }
 
 
 
-int print_array(unsigned int * ptr )
+void print_statistics(uchar_t* ptr)
 {
-    for(int i=0;i<SIZE;i++)
+    printf("\nMax = %d\n",find_maximum(ptr));
+    printf("Min = %d\n",find_minimum(ptr));
+    printf("Mean = %d\n",find_mean(ptr));
+    printf("Median = %d\n",find_median(ptr));
+
+}
+
+
+void print_array(uchar_t* ptr)
+{
+    for(size_t i=0;i<SIZE;i++)
     {
-       printf("%d-%d\n",i+1,*ptr);
+       printf("%d\n",*ptr);
        ptr++ ;
     }
 
-    return 0 ;
+}
+
+
+uchar_t find_median(uchar_t* ptr)
+{
+    size_t i;
+    if(SIZE%2==0)
+    {
+        for(i=1;i<(SIZE/2);i++)
+        {ptr++;}
+        return( (*ptr+*(ptr+1))/2 );
+    }
+        else
+    {
+        for(i=1;i<=(SIZE/2);i++)
+        {ptr++;}
+        return(*ptr) ;
+    }
 
 }
 
 
-int find_maximum(unsigned int * ptr1)
+uchar_t find_mean(uchar_t* ptr)
 {
-    int max = *ptr1 ;
-    for(int i=1;i<SIZE;i++)
+    size_t sum=0 ;
+    for(size_t i=0;i<SIZE;i++)
     {
-        ptr1++;
-        if(*ptr1>max)
-        {
-            max = *ptr1 ;
-        }
+        sum += *ptr;
+        ptr++;
     }
-    return(max);
-}
 
-
-int find_minimum(unsigned int * ptr2)
-{
-    int min = *ptr2 ;
-    for(int i=1;i<SIZE;i++)
-    {
-        ptr2++;
-        if(*ptr2<min)
-        {
-            min = *ptr2 ;
-        }
-    }
-    return(min);
-}
-
-
-int find_mean(unsigned int * ptr3)
-{
-    int sum=0 ;
-    for(int i=0;i<SIZE;i++)
-    {
-        sum += *ptr3 ;
-        ptr3++;
-    }
     return(sum/SIZE);
 }
 
 
-int find_median(unsigned int * ptr4)
+uchar_t find_maximum(uchar_t* ptr)
 {
-    int median , temp=0 ;
-    if(SIZE%2==0)
+    size_t max = *ptr ;
+    for(size_t i=1;i<SIZE;i++)
     {
-        for(int i=1;i<(SIZE/2);i++)
-        {ptr4++;}
-        temp += *ptr4 ;
-        ptr4++ ;
-        median = (temp+*ptr4)/2;
+        ptr++;
+        if(*ptr>max)
+        {
+            max = *ptr;
+        }
     }
-        else
-    {
-        for(int j=1;j<=(SIZE/2);j++)
-        {ptr4++;}
-        median = *ptr4 ;
-    }
-        return(median);
+
+    return(max);
 }
 
 
-int sort_array(unsigned int * ptr5)
+uchar_t find_minimum(uchar_t* ptr)
 {
-    unsigned int temp[SIZE] , hold   ;
-    for(int i=0;i<SIZE;i++)
+    int min = *ptr ;
+    for(size_t i=1;i<SIZE;i++)
     {
-        temp[i]=*ptr5 ;
-        ptr5++;
-    }
-    for(int j=0;j<SIZE;j++)
-    {
-        for(int k=j+1;k<SIZE;k++)
+        ptr++;
+        if(*ptr<min)
         {
-            if(temp[k]>temp[j])
+            min = *ptr;
+        }
+    }
+
+    return(min);
+}
+
+
+void sort_array(uchar_t* ptr)
+{
+    uchar_t temp;
+    for(size_t i=0;i<SIZE;i++)
+    {
+        for(size_t j=i+1;j<SIZE;j++)
+        {
+            if(*(ptr+j)>*(ptr+i))
             {
-                hold = temp[j] ;
-                temp[j]=temp[k];
-                temp[k] = hold ;
+                temp = *(ptr+i) ;
+                *(ptr+i)=*(ptr+j);
+                *(ptr+j)= temp ;
             }
         }
     }
-    print_array(&temp[0]);
 
-    return 0 ;
-
-}
-
-
-int print_statistics(unsigned int * ptr6)
-{
-    printf("\nMax = %d\n",find_maximum(ptr6));
-    printf("Min = %d\n",find_minimum(ptr6));
-    printf("Mean = %d\n",find_mean(ptr6));
-    printf("Median = %d\n",find_median(ptr6));
-
-    return 0 ;
 }
--- c1/stats.h
+++ c1/stats.h
@@ -2,105 +2,19 @@
 #define __STATS_H__
 /********************************************************************
 *
-* Function : print_array
-*
-* Description :
-*     take the given array and print it on screen .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     printed array on the screen .
+* typedef : uchar_t
+*     ease of use for unsigned char
 *
 ********************************************************************/
-int print_array(unsigned int * ptr) ;
+typedef unsigned char uchar_t;
 
 /********************************************************************
 *
-* Function : find_maximum
-*
-* Description :
-*     take the given array and find it's maximum .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     maximum of the given array .
+* Constants :   SIZE
+*     size of the array
 *
 ********************************************************************/
-int find_maximum(unsigned int * ptr1) ;
-
-/********************************************************************
-*
-* Function : find_minimum
-*
-* Description :
-*     take the given array and find it's minimum .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     minimum of the given array .
-*
-********************************************************************/
-int find_minimum(unsigned int * ptr2) ;
-
-/********************************************************************
-*
-* Function : find_mean
-*
-* Description :
-*     take the given array and find it's mean .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     mean of the given array .
-*
-********************************************************************/
-int find_mean(unsigned int * ptr3) ;
-
-/********************************************************************
-*
-* Function : find_median
-*
-* Description :
-*     take the given array and find it's median .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     median of the given array .
-*
-********************************************************************/
-int find_median(unsigned int * ptr4) ;
-
-/********************************************************************
-*
-* Function : sort_array
-*
-* Description :
-*     take the given array and print it on screen .
-*
-* Parameters :
-*     int * ptr : pointer to the given array .
-*     int SIZE  : number of items in the array .
-*
-* Return :
-*     printed array on the screen .
-*
-********************************************************************/
-int sort_array(unsigned int * ptr5) ;
+#define SIZE 40
 
 /********************************************************************
 *
@@ -118,14 +32,108 @@ int sort_array(unsigned int * ptr5) ;
 *     printed statistics of the array to the screen .
 *
 ********************************************************************/
-int print_statistics(unsigned int * ptr6) ;
+void print_statistics(uchar_t* ptr) ;
 
 /********************************************************************
 *
-* Constants :
-*     size of the array
+* Function : print_array
+*
+* Description :
+*     take the given array and print it on screen .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     printed array on the screen .
 *
 ********************************************************************/
-#define SIZE 40
+void print_array(uchar_t* ptr) ;
+
+/********************************************************************
+*
+* Function : find_median
+*
+* Description :
+*     take the given array and find it's median .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     median of the given array .
+*
+********************************************************************/
+uchar_t find_median(uchar_t* ptr) ;
+
+/********************************************************************
+*
+* Function : find_mean
+*
+* Description :
+*     take the given array and find it's mean .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     mean of the given array .
+*
+********************************************************************/
+uchar_t find_mean(uchar_t* ptr) ;
+
+/********************************************************************
+*
+* Function : find_maximum
+*
+* Description :
+*     take the given array and find it's maximum .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     maximum of the given array .
+*
+********************************************************************/
+uchar_t find_maximum(uchar_t* ptr) ;
+
+/********************************************************************
+*
+* Function : find_minimum
+*
+* Description :
+*     take the given array and find it's minimum .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     minimum of the given array .
+*
+********************************************************************/
+uchar_t find_minimum(uchar_t* ptr) ;
+
+/********************************************************************
+*
+* Function : sort_array
+*
+* Description :
+*     take the given array and print it on screen .
+*
+* Parameters :
+*     int * ptr : pointer to the given array .
+*     int SIZE  : number of items in the array .
+*
+* Return :
+*     printed array on the screen .
+*
+********************************************************************/
+void sort_array(uchar_t* ptr) ;
 
 #endif /* __STATS_H__ */
